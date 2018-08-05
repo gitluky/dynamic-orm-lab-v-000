ba50c41c0fe75ebabfdb3d7b9bb2f767f3438aac
Jonathan Luk <jluk87@gmail.com>
2018-08-05 22:32:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,7 +40,7 @@ class InteractiveRecord
   def values_for_insert
     self.class.column_names.collect do |column_name|
       "#{self.send(column_name)}" unless self.send("#{column_name}").nil?
-    end.join(", ")
+    end.reject {|value| value.nil?}.join(", ")
   end
 
   def save
