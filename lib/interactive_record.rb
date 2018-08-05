d9bf03231cf2a333db53690e885a82544a753b5c
Jonathan Luk <jluk87@gmail.com>
2018-08-05 21:28:19 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,20 +4,27 @@ require 'active_support/inflector'
 class InteractiveRecord
 
   def self.table_name
-
-
+    self.to_s.downcase.pluralize
   end
 
 
-  def self.columns_names
-
-
+  def self.column_names
+    sql = <<-SQL
+      SELECT PRAGMA table_info (#{self.table_name})
+    SQL
+    
+    table_columns = []
+    
+    DB[:conn].execute(sql).each do |column|
+      table_columns << column["name"]
+    end
+    
   end
 
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
-    end  
+    end
   end
-  
+
 end
