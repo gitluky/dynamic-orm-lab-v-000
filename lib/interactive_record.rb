b3f3bcd0c3220dd4b1bc119514f4418568c7c8b6
Jonathan Luk <jluk87@gmail.com>
2018-08-05 22:54:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ class InteractiveRecord
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM #{self.table_name}
-      WHERE name = #{name}
+      WHERE name = "#{name}"
     SQL
 
     DB[:conn].execute(sql)
