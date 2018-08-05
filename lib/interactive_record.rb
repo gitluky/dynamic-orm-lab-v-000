1539d74c2b9fe5b51e97bb38b5a68b4e88f3abb8
Jonathan Luk <jluk87@gmail.com>
2018-08-05 21:22:05 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,22 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+
+
+  end
+
+
+  def self.columns_names
+
+
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end  
+  end
   
 end
