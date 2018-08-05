408ca2beae46101f9a6b8467f30e13b3e2a747b0
Jonathan Luk <jluk87@gmail.com>
2018-08-05 22:23:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,7 +41,7 @@ class InteractiveRecord
     binding.pry
     self.class.column_names.collect do |column_name|
       "#{self.send(column_name)}"
-    end.compact.join(", ")
+    end.reject(" ").join(", ")
 
   end
 
